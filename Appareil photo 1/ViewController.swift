4cb73e70416bf13e4e98bb69b0691da83ac2b426
Kevin Trebossen <kevintrebossen@imac-de-kevin.local>
2018-09-19 22:57:31 +0200

    Gestion des erreurs si il n'y a pas d'appareil photo et gestion des autres alertes

--- Appareil photo 1/ViewController.swift
+++ Appareil photo 1/ViewController.swift
@@ -21,8 +21,41 @@ class ViewController: UIViewController {
         imagePicker.delegate = self
         imagePicker.allowsEditing = true
     }
+    
+    func presentWithSource(_ source: UIImagePickerController.SourceType) {      // source que l'on doit envoyer
+        imagePicker.sourceType = source
+        present(imagePicker,animated: true,completion: nil)
+        
+        
+    }
 
     @IBAction func prendrePhoto(_ sender: UIButton) {
+        let alerteActionSheet = UIAlertController(title: "Prendre une photo", message: "Choisissez le média", preferredStyle: .actionSheet)
+        let camera = UIAlertAction(title: "Appareil photo", style: .default) { (action) in
+            if UIImagePickerController.isSourceTypeAvailable(.camera) {                     // on vérifie si u appareil photo est disponible
+                self.presentWithSource(.camera)
+            } else {                                                                        // Si il n'y a pas d'appareil photo disponible
+                let alerte = UIAlertController(title: "Erreur", message: "Aucun appareil photo n'est disponible", preferredStyle: .alert)
+                let annuler = UIAlertAction(title: "Je comprends", style: .destructive, handler: nil)
+                alerte.addAction(annuler)
+                self.present(alerte, animated: true, completion: nil)
+            }
+        }
+        let gallerie = UIAlertAction(title: "Galerie de photos", style: .default) { (action) in
+            self.presentWithSource(.photoLibrary)
+        }
+        let cancel = UIAlertAction(title: "Annuler", style: .cancel, handler: nil)
+        alerteActionSheet.addAction(camera)
+        alerteActionSheet.addAction(gallerie)
+        alerteActionSheet.addAction(cancel)
+        
+        if let popover = alerteActionSheet.popoverPresentationController {      // pour empecher un crash sur iPad
+            popover.sourceView = view
+            popover.sourceRect = CGRect(x: view.frame.midX, y: view.frame.midY, width: 0, height: 0)
+            popover.permittedArrowDirections = []
+        }
+        
+        present(alerteActionSheet,animated: true,completion: nil)
     }
     
 }
