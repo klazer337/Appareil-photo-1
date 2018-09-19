dd4075a95fb32a2777dbf4421ca22af7da1d9c80
Kevin Trebossen <kevintrebossen@imac-de-kevin.local>
2018-09-19 22:39:02 +0200

    Commit UIViewController + privacy (Plist)

--- Appareil photo 1/ViewController.swift
+++ Appareil photo 1/ViewController.swift
@@ -13,12 +13,34 @@ class ViewController: UIViewController {
     @IBOutlet weak var imageViewChoisi: UIImageView!
     @IBOutlet weak var nosImagesLabel: UILabel!
     
+    var imagePicker = UIImagePickerController()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
     }
 
     @IBAction func prendrePhoto(_ sender: UIButton) {
+    }
+    
+}
+
+
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let edite = info[.editedImage] as? UIImage {     // image éditée
+            imageViewChoisi.image = edite
+        } else if let originale = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {     // ou image originale
+            imageViewChoisi.image = originale
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
     }
     
 }
