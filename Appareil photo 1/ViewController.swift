8d45d7675310111daa50fee3b9a7978b562c74ed
Kevin Trebossen <kevintrebossen@imac-de-kevin.local>
2018-09-19 22:27:50 +0200

    Initial commit + Creation UI

--- Appareil photo 1/ViewController.swift
+++ Appareil photo 1/ViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageViewChoisi: UIImageView!
+    @IBOutlet weak var nosImagesLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func prendrePhoto(_ sender: UIButton) {
+    }
+    
 }
 
