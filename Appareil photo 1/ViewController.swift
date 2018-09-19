db5bd9aa051af39a3733282a575b86c9871e1349
Kevin Trebossen <kevintrebossen@imac-de-kevin.local>
2018-09-19 22:19:08 +0200

    Initial Commit

---
+++ Appareil photo 1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Appareil photo 1
+//
+//  Created by Kevin Trebossen on 19/09/18.
+//  Copyright © 2018 KTD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
